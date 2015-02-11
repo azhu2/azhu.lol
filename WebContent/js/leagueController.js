709f8ad51b5508f5c4a3109d14eefa59ba8b3a0a
azhu2 <azhu2@illinois.edu>
2015-02-11 17:41:02 -0600

    Game length formatting

--- WebContent/js/leagueController.js
+++ WebContent/js/leagueController.js
@@ -10,142 +10,144 @@ leagueApp.config(function($routeProvider) {
 	});
 });
 
-leagueApp.controller('lookupController',
-	function($scope, LeagueResource) {
-		$scope.version = "5.2.2";
-		$scope.showLookupResults = false;
+leagueApp.controller('lookupController', function($scope, LeagueResource) {
+	$scope.version = "5.2.2";
+	$scope.showLookupResults = false;
+	$scope.showRankedResults = false;
+	$scope.showMatchHIstory = false;
+
+	$scope.lookup = function() {
+		LeagueResource.lookupSummoner().get({
+			name : $scope.summonerName
+		}, function(data) {
+			$scope.summoner = data;
+		});
+		$scope.showLookupResults = true;
 		$scope.showRankedResults = false;
-		$scope.showMatchHIstory = false;
-		
-		$scope.lookup = function() {
-			LeagueResource.lookupSummoner().get({
-				name : $scope.summonerName
-			}, function(data) {
-				$scope.summoner = data;
-			});
-			$scope.showLookupResults = true;
-			$scope.showRankedResults = false;
-			$scope.showMatchHistory = false;
-		};
+		$scope.showMatchHistory = false;
+	};
 
-		$scope.lookupRanked = function(summonerId) {
-			$scope.rankedData = [];
-			LeagueResource.lookupRanked(summonerId).query({
-				id : summonerId
-			}, function(data) {
-				for (var i = 0; i < data.length; i++) {
-					parseRankedGame($scope, data[i], data.length - i - 1);
-				}
-			});
-			$scope.showRankedResults = true;
-			$scope.showMatchHistory = false;
-		};
-
-		var parseRankedGame = function($scope, match, index) {
-			LeagueResource.champFromId().get({
-				id : match.participants[0].championId
-			}, function(champData) {
-				match.champion = champData;
-				$scope.rankedData[index] = match;
-			});
-		};
-
-		$scope.lookupMatches = function(summonerId) {
-			$scope.matchData = [];
-			LeagueResource.lookupMatches(summonerId).query({
-				id : summonerId
-			}, function(data) {
-				for (var i = 0; i < data.length; i++) {
-					parseGame($scope, data[i], data.length - i - 1);
-				}
-			});
-			$scope.showRankedResults = false;
-			$scope.showMatchHistory = true;
-		};
-
-		var parseGame = function($scope, match, index) {
-			LeagueResource.champFromId().get({
-				id : match.championId
-			}, function(champData) {
-				match.champion = champData;
-			});
-			
-			LeagueResource.summSpellFromId().get({
-				id : match.spell1
-			}, function(champData) {
-				match.sumSpell1 = champData;
-			});
-
-			LeagueResource.summSpellFromId().get({
-				id : match.spell2
-			}, function(champData) {
-				match.sumSpell2 = champData;
-			});
-			
-			$scope.matchData[index] = match;
-		};
-
-		$scope.expandMatch = function(match, summoner) {
-			if (match.showExpand) {
-				match.showExpand = false;
-				return;
+	$scope.lookupRanked = function(summonerId) {
+		$scope.rankedData = [];
+		LeagueResource.lookupRanked(summonerId).query({
+			id : summonerId
+		}, function(data) {
+			for (var i = 0; i < data.length; i++) {
+				parseRankedGame($scope, data[i], data.length - i - 1);
 			}
+		});
+		$scope.showRankedResults = true;
+		$scope.showMatchHistory = false;
+	};
 
-			closeAllMatches($scope);
-			match.showExpand = true;
+	var parseRankedGame = function($scope, match, index) {
+		LeagueResource.champFromId().get({
+			id : match.participants[0].championId
+		}, function(champData) {
+			match.champion = champData;
+			$scope.rankedData[index] = match;
+		});
+	};
 
-			$scope.matchPlayerBlue = [];
-			$scope.matchPlayerRed = [];
-			for (var i = 0; i < match.fellowPlayers.length; i++) {
-				parsePlayer($scope, match.fellowPlayers[i], match.fellowPlayers.length
-					- i - 1);
+	$scope.lookupMatches = function(summonerId) {
+		$scope.matchData = [];
+		LeagueResource.lookupMatches(summonerId).query({
+			id : summonerId
+		}, function(data) {
+			for (var i = 0; i < data.length; i++) {
+				parseGame($scope, data[i], data.length - i - 1);
 			}
+		});
+		$scope.showRankedResults = false;
+		$scope.showMatchHistory = true;
+	};
 
-			// Add current player
-			var player = {champ : match.champion, summoner : summoner};
-			if(match.teamId === 100)
-				$scope.matchPlayerBlue.push(player);
-			else
-				$scope.matchPlayerRed.push(player);
-			
-			// lookupMatch($scope, match); Only useful for ranked
+	var parseGame = function($scope, match, index) {
+		LeagueResource.champFromId().get({
+			id : match.championId
+		}, function(champData) {
+			match.champion = champData;
+		});
+
+		LeagueResource.summSpellFromId().get({
+			id : match.spell1
+		}, function(champData) {
+			match.sumSpell1 = champData;
+		});
+
+		LeagueResource.summSpellFromId().get({
+			id : match.spell2
+		}, function(champData) {
+			match.sumSpell2 = champData;
+		});
+
+		$scope.matchData[index] = match;
+	};
+
+	$scope.expandMatch = function(match, summoner) {
+		if (match.showExpand) {
+			match.showExpand = false;
+			return;
+		}
+
+		closeAllMatches($scope);
+		match.showExpand = true;
+
+		$scope.matchPlayerBlue = [];
+		$scope.matchPlayerRed = [];
+		for (var i = 0; i < match.fellowPlayers.length; i++) {
+			parsePlayer($scope, match.fellowPlayers[i], match.fellowPlayers.length - i
+				- 1);
+		}
+
+		// Add current player
+		var player = {
+			champ : match.champion,
+			summoner : summoner
 		};
+		if (match.teamId === 100)
+			$scope.matchPlayerBlue.push(player);
+		else
+			$scope.matchPlayerRed.push(player);
 
-		var parsePlayer = function($scope, player, index) {
-			var playerData = {};
+		// lookupMatch($scope, match); Only useful for ranked
+	};
 
-			LeagueResource.summonerFromId().get({
-				id : player.summonerId
-			}, function(summonerData) {
-				playerData.summoner = summonerData;
-			});
-			
-			LeagueResource.champFromId().get({
-				id : player.championId
-			}, function(champData) {
-				playerData.champ = champData;
-			});
+	var parsePlayer = function($scope, player, index) {
+		var playerData = {};
 
-			if(player.teamId === 100)
-				$scope.matchPlayerBlue.push(playerData);
-			else
-				$scope.matchPlayerRed.push(playerData);
-		};
+		LeagueResource.summonerFromId().get({
+			id : player.summonerId
+		}, function(summonerData) {
+			playerData.summoner = summonerData;
+		});
 
-		var lookupMatch = function($scope, match) {
-			LeagueResource.matchDetail().get({
-				id : match.gameId
-			}, function(matchData) {
-				$scope.matchDetails = matchData;
-			});
-		};
+		LeagueResource.champFromId().get({
+			id : player.championId
+		}, function(champData) {
+			playerData.champ = champData;
+		});
 
-		var closeAllMatches = function($scope) {
-			for (var i = 0; i < $scope.matchData.length; i++) {
-				$scope.matchData[i].showExpand = false;
-			}
-		};
-	});
+		if (player.teamId === 100)
+			$scope.matchPlayerBlue.push(playerData);
+		else
+			$scope.matchPlayerRed.push(playerData);
+	};
+
+	var lookupMatch = function($scope, match) {
+		LeagueResource.matchDetail().get({
+			id : match.gameId
+		}, function(matchData) {
+			$scope.matchDetails = matchData;
+		});
+	};
+
+	var closeAllMatches = function($scope) {
+		for (var i = 0; i < $scope.matchData.length; i++) {
+			$scope.matchData[i].showExpand = false;
+		}
+	};
+});
 
 leagueApp.service('LeagueResource', function($resource) {
 	this.lookupSummoner = function() {
@@ -171,7 +173,7 @@ leagueApp.service('LeagueResource', function($resource) {
 	this.matchDetail = function() {
 		return $resource('/azhu.lol/rest/match/:id');
 	};
-	
+
 	this.summSpellFromId = function() {
 		return $resource('/azhu.lol/rest/summoner-spell/:id');
 	}
@@ -198,7 +200,21 @@ leagueApp.filter("queueFilter", function() {
 		filtered = filtered.replace("NONE", "Custom");
 		return filtered;
 	};
+}).filter('numberFixedLen', function() {
+	return function(n, len) {
+		var num = parseInt(n, 10);
+		len = parseInt(len, 10);
+		if (isNaN(num) || isNaN(len)) {
+			return n;
+		}
+		num = '' + num;
+		while (num.length < len) {
+			num = '0' + num;
+		}
+		return num;
+	};
 });
+;
 
 leagueApp.directive('errSrc', function() {
 	return {
