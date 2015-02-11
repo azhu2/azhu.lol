e2102fd2d161119c7cb224bda981c2df4abff445
azhu2 <azhu2@illinois.edu>
2015-02-11 16:47:57 -0600

    Actually add constnats file

---
+++ src/league/api/LeagueAPIConstants.java
@@ -0,0 +1,12 @@
+package league.api;
+
+public class LeagueAPIConstants{
+
+    public static final int HTTP_OK = 200;
+    public static final int HTTP_UNAUTHORIZED = 401;
+    public static final int HTTP_NOT_FOUND = 404;
+    public static final int HTTP_RATELIMIT = 429;
+    public static final int HTTP_INTERNAL_SERVER_ERROR = 500;
+    public static final int HTTP_UNAVAILABLE = 503;
+
+}
