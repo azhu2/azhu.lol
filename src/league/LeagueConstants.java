cb84258b6a3fe53b23283ed22687111d21876d1e
azhu2 <azhu2@illinois.edu>
2015-03-03 22:10:56 -0600

    Constants file

---
+++ src/league/LeagueConstants.java
@@ -0,0 +1,7 @@
+package league;
+
+public class LeagueConstants{
+    public static final int BLUE_TEAM = 100;
+    public static final int RED_TEAM = 200;
+    public static final String SOLO_QUEUE_5v5 = "RANKED_SOLO_5x5";
+}
